dd26ace7474a4e4c92215fea556b208553dac464
ambiti0n <fabulousfresh010@gmail.com>
2024-04-29 04:09:37 +0100

    chore: integrate supabase

---
+++ app/api/user.js
@@ -0,0 +1,31 @@
+import { supabase } from "../../lib/supabase";
+
+export default async function handler(req, res) {
+  if (req.method === "POST") {
+    const { data, error } = await supabase
+      .from("your_table_name")
+      .insert([req.body]);
+
+    if (error) {
+      return res.status(500).json({ error: error.message });
+    }
+
+    res.status(200).json(data);
+  } else {
+    res.setHeader("Allow", ["POST"]);
+    res.status(405).end(`Method ${req.method} Not Allowed`);
+  }
+}
+
+export async function getServerSideProps(context) {
+  const { data, error } = await supabase.from("user").select("*");
+
+  console.log({ data });
+
+  if (error) {
+    console.error(error);
+    return { props: { error: error.message } };
+  }
+
+  return { props: { data } };
+}
