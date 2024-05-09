ec19f35d870420b213f3a132116f91fd84f9f647
Ololade Otemade <adeniyiotemade@gmail.com>
2024-05-09 20:41:25 +0100

    Global context

---
+++ frontend/app/context/GlobalContext.js
@@ -0,0 +1,25 @@
+// globalContext.js (Global Context)
+import React, { createContext, useContext, useState } from 'react';
+import { OnboardingContext } from '../onboarding/page';
+
+export const GlobalContext = createContext();
+
+export const GlobalProvider = ({ children }) => {
+  const [globalState, setGlobalState] = useState({});
+
+  // Accessing state from AuthContext
+  const { isLoggedIn, user, state } = useContext(OnboardingContext);
+
+  // You can perform any actions with auth state here and update globalState accordingly
+
+  // Example: If user is logged in, update global state
+  if (isLoggedIn && user) {
+    setGlobalState({ ...globalState, user });
+  }
+
+  return (
+    <GlobalContext.Provider value={globalState}>
+      {children}
+    </GlobalContext.Provider>
+  );
+};
--- frontend/app/onboarding/page.js
+++ frontend/app/onboarding/page.js
@@ -5,6 +5,7 @@ import styles from "./onboarding.module.scss";
 import Image from "next/image";
 import Signin from "../components/auth/Signin";
 import Signup from "../components/auth/Signup";
+import { GlobalProvider } from "../context/GlobalContext";
 
 export const OnboardingContext = createContext();
 
@@ -99,41 +100,43 @@ const Onboarding = () => {
         state,
       }}
     >
-      <div className={styles.container}>
-        <div className={styles.left}>
-          <div className="x-axis gap-1">
-            <Image
-              src="/icons/xalariIcon.png"
-              alt="Brand icon"
-              width={50}
-              height={50}
-            />
-            <h1>Xalari</h1>
+      <GlobalProvider>
+        <div className={styles.container}>
+          <div className={styles.left}>
+            <div className="x-axis gap-1">
+              <Image
+                src="/icons/xalariIcon.png"
+                alt="Brand icon"
+                width={50}
+                height={50}
+              />
+              <h1>Xalari</h1>
+            </div>
+            <div>
+              <p className={styles.title}>
+                Payroll <br />
+                Management on the <br />
+                blockchain!
+              </p>
+              <p className={styles.desc}>
+                Revolutionize Payroll Management with Blockchain Technology!
+                Discover a secure, transparent, and efficient way to handle
+                payroll, ensuring accuracy, trust, and simplicity for businesses
+                of all sizes.
+              </p>
+            </div>
+            <div className={styles.asset}>
+              <Image
+                src="/images/calculator.png"
+                alt="Calculator image"
+                width={400}
+                height={350}
+              />
+            </div>
           </div>
-          <div>
-            <p className={styles.title}>
-              Payroll <br />
-              Management on the <br />
-              blockchain!
-            </p>
-            <p className={styles.desc}>
-              Revolutionize Payroll Management with Blockchain Technology!
-              Discover a secure, transparent, and efficient way to handle
-              payroll, ensuring accuracy, trust, and simplicity for businesses
-              of all sizes.
-            </p>
-          </div>
-          <div className={styles.asset}>
-            <Image
-              src="/images/calculator.png"
-              alt="Calculator image"
-              width={400}
-              height={350}
-            />
-          </div>
+          <div className={styles.right}>{renderPages()}</div>
         </div>
-        <div className={styles.right}>{renderPages()}</div>
-      </div>
+      </GlobalProvider>
     </OnboardingContext.Provider>
   );
 };
