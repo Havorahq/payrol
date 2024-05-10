69ffba52c33421f8e397238317f162565df3a6e3
Ololade Otemade <adeniyiotemade@gmail.com>
2024-05-10 21:48:43 +0100

    UI fix

--- frontend/app/(dashboard)/[id]/page.tsx
+++ frontend/app/(dashboard)/[id]/page.tsx
@@ -1,5 +1,5 @@
-import Button from '@/app/components/common/Button';
-import Wrapper from '@/app/components/wrapper/Wrapper'
+import Button from '../../components/common/Button';
+import Wrapper from '../../components/wrapper/Wrapper'
 import React from 'react'
 import styles from './styles.module.scss'
 import { capitalizeFirst } from '@/plugins/utils';
