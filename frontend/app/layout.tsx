64d597be7d1265087c8e83ff3b7ae8902f69b9ce
ambiti0n <fabulousfresh010@gmail.com>
2024-07-01 15:37:32 +0100

    chore: fix react

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,5 +1,5 @@
 "use client";
-
+import React from "react";
 import "@rainbow-me/rainbowkit/styles.css";
 import { getDefaultWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit";
 import { configureChains, createConfig, WagmiConfig } from "wagmi";
