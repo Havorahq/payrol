3c3b740db72d9a2091c313e71e150b433f3063a6
ambiti0n <fabulousfresh010@gmail.com>
2024-07-04 00:05:53 +0100

    chore react query

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -6,10 +6,9 @@ import React from "react";
 // import { alchemyProvider } from "wagmi/providers/alchemy";
 // import { publicProvider } from "wagmi/providers/public";
 // import { lisk_testnet } from "../lib/network";
-// import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-// import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-// const queryClient = new QueryClient();
+const queryClient = new QueryClient();
 
 // const { chains, publicClient } = configureChains(
 //   [lisk_testnet],
@@ -40,14 +39,13 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        {/* <QueryClientProvider client={queryClient}> */}
-        {/* <WagmiConfig config={wagmiConfig}>
-          <RainbowKitProvider chains={chains} initialChain={lisk_testnet.id}> */}
-        {children}
-        {/* </RainbowKitProvider>
-        </WagmiConfig> */}
-        {/* <ReactQueryDevtools initialIsOpen={false} />
-        </QueryClientProvider> */}
+        <QueryClientProvider client={queryClient}>
+          {/* <WagmiConfig config={wagmiConfig}>
+            <RainbowKitProvider chains={chains} initialChain={lisk_testnet.id}> */}
+          {children}
+          {/* </RainbowKitProvider>
+          </WagmiConfig> */}
+        </QueryClientProvider>
       </body>
     </html>
   );
