f15edf22f189afb76c08f81449f3ecca0335ca7f
ambiti0n <fabulousfresh010@gmail.com>
2024-08-17 14:49:52 +0100

    feat: integrate dynamic eallet connect

--- frontend/app/components/auth/Signin.tsx
+++ frontend/app/components/auth/Signin.tsx
@@ -1,17 +1,17 @@
 import React, { useState, useContext, useEffect } from "react";
 import { OnboardingContext } from "../../contexts/OnboardingContext";
 import Button from "../common/Button";
-// import { ConnectButton } from "@rainbow-me/rainbowkit";
-// import { useAccount, useDisconnect } from "wagmi";
+import { useDynamicContext } from "@dynamic-labs/sdk-react-core";
 import { useRouter } from "next/navigation";
 import { findUser } from "../../api/helper-functions";
+import { DynamicWidget } from "../../../lib/dynamic";
 const Signin: React.FC = () => {
   const [isLoading, setIsLoading] = useState(true);
   const context = useContext(OnboardingContext);
 
-  // const account = useAccount();
-  // const { address } = account;
-  // const { disconnect } = useDisconnect();
+  const { user } = useDynamicContext();
+
+  console.log("Logged in user", user);
   const router = useRouter();
 
   // useEffect(() => {
@@ -51,12 +51,23 @@ const Signin: React.FC = () => {
     <div className="flex flex-col m-0 px-8 py-1 h-fit justify-center overflow-y-scroll gap-16">
       <div className="flex flex-col items-center">
         <h1>Welcome 👋🏼</h1>
-        <p className="text-sm">Please sign in to your account</p>
+        <p className="text-sm">Please sign in to your account </p>
+
         <p className="mt-2 text-gray-400 text-xs">
           You will be redirected to the sign up page if you don&apos;t have an
           account
         </p>
-        <div className="flex justify-center">{/* <ConnectButton /> */}</div>
+        <div className="flex justify-center">
+     
+          <DynamicWidget
+            innerButtonComponent={
+              <Button>My Custom Connect Wallet Button</Button>
+            }
+          >
+        
+          </DynamicWidget>
+          
+        </div>
       </div>
       <p className="self-center text-sm">
         Need to create an account?{" "}
