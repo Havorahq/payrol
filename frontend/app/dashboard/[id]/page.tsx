fa2f611ac7b3b981d16be556e5fd98d2d110b0e6
ambiti0n <fabulousfresh010@gmail.com>
2024-08-24 19:58:11 +0100

    chore: fix sc

--- frontend/app/dashboard/[id]/page.tsx
+++ frontend/app/dashboard/[id]/page.tsx
@@ -8,6 +8,8 @@ import Modal from "../../components/common/Modal";
 import { capitalizeFirst } from "../../../plugins/utils";
 import useContractData from "../../hooks/useContractData";
 import useUserData from "../../hooks/useUserData";
+import { useWriteContract } from "wagmi";
+import { contractAddress } from "../../../lib/contract/contract";
 
 const ContractDetail = () => {
   const router = useRouter();
@@ -19,6 +21,8 @@ const ContractDetail = () => {
   const [acceptingPaymentClicked, setAcceptingPaymentClicked] = useState(false);
   const { userData, isLoading: userLoading, error: userErroer } = useUserData();
   const { contracts, isLoading, error } = useContractData();
+  const abi = require("../../../lib/contract/contract_abi.json");
+  const { writeContract } = useWriteContract();
 
   useEffect(() => {
     if (id && contracts) {
@@ -42,109 +46,126 @@ const ContractDetail = () => {
     openModal();
   };
 
+  const contractFunction = () => {
+    return writeContract({
+      abi,
+      address: contractAddress,
+      functionName: "transferFrom",
+      args: [
+        "0xd2135CfB216b74109775236E36d4b433F1DF507B",
+        "0xA0Cf798816D4b9b9866b5330EEa46a18382f251e",
+      ],
+    });
+  };
+
   return (
-    <Wrapper>
-      <Modal isOpen={isOpen} onClose={closeModal}>
-        <div className="p-6">
-          <h2 className="text-xl font-bold">All done! 🎉</h2>
-          <p className="text-sm text-gray-500 my-2">
-            You have Confirmed your payment!
-          </p>
-          <Button
-            label="Back to Home"
-            onClick={() => router.push("/dashboard")}
-          />
-        </div>
-      </Modal>
-      <div className="p-10 text-[#131414] mt-3">
-        <h1 className="text-2xl font-bold text-left mb-4">
-          Contract Details 📄
-        </h1>
-        <p className="text-gray-600 mt-2">Details of your contract below</p>
-        <div className="border-b py-4">
-          <p className="font-medium">Contract Type</p>
-          <p className="text-gray-600">
-            {capitalizeFirst(contract ? contract.contract_type : "")}
-          </p>
-        </div>
-        <div className="border-b py-4">
-          <p className="font-medium">Employee Email</p>
-          <p className="text-gray-600">
-            {contract ? contract.employee_id : ""}
-          </p>
-        </div>
-        <div className="border-b py-4">
-          <p className="font-medium">Job Title</p>
-          <p className="text-gray-600">{contract ? contract.job_title : ""}</p>
-        </div>
-        <div className="border-b py-4">
-          <p className="font-medium">Job Description</p>
-          <p className="text-gray-600">
-            {contract ? contract.job_description : ""}
-          </p>
-        </div>
-        <div className="border-b py-4">
-          <p className="font-medium">Rate ($)</p>
-          <p className="text-gray-600">{contract ? contract.payment : ""}</p>
-        </div>
-        <div className="border-b py-4">
-          <p className="font-medium">Employee Wallet Address</p>
-          <p className="text-gray-600">
-            {contract ? contract.payment_address : ""}
-          </p>
-        </div>
-        <div className="border-b py-4">
-          <p className="font-medium">Contract Status</p>
-          <p className="text-gray-600">
-            {contract ? capitalizeFirst(contract.status) : ""}
-          </p>
-        </div>
-        {isEmployer &&
-          contract &&
-          contract.status === "active" &&
-          contract.payment_status !== "pending" &&
-          contract.payment_status !== "active" && (
-            <div className="my-2">
-              <p className="font-medium">Payment Hash</p>
-              <input
-                type="text"
-                placeholder="Submit Blockchain payment Hash"
-                className="w-full border rounded p-2"
-              />
-              <Button
-                label="Submit Payment Hash"
-                onClick={() => handleSendPayment("send")}
-                style="mt-2"
-              />
-            </div>
-          )}
-        {!isEmployer && (
-          <div>
-            {acceptingPaymentClicked ? (
-              <p>Processing acceptance...</p>
-            ) : (
-              <div>
-                {contract && contract.status === "pending" && (
-                  <Button
-                    label="Accept Contract"
-                    onClick={handleEnterAgreement}
-                  />
-                )}
+    <>
+      <button onClick={() => contractFunction()}></button>
+      <Wrapper>
+        <Modal isOpen={isOpen} onClose={closeModal}>
+          <div className="p-6">
+            <h2 className="text-xl font-bold">All done! 🎉</h2>
+            <p className="text-sm text-gray-500 my-2">
+              You have Confirmed your payment!
+            </p>
+            <Button
+              label="Back to Home"
+              onClick={() => router.push("/dashboard")}
+            />
+          </div>
+        </Modal>
+        <div className="p-10 text-[#131414] mt-3">
+          <h1 className="text-2xl font-bold text-left mb-4">
+            Contract Details 📄
+          </h1>
+          <p className="text-gray-600 mt-2">Details of your contract below</p>
+          <div className="border-b py-4">
+            <p className="font-medium">Contract Type</p>
+            <p className="text-gray-600">
+              {capitalizeFirst(contract ? contract.contract_type : "")}
+            </p>
+          </div>
+          <div className="border-b py-4">
+            <p className="font-medium">Employee Email</p>
+            <p className="text-gray-600">
+              {contract ? contract.employee_id : ""}
+            </p>
+          </div>
+          <div className="border-b py-4">
+            <p className="font-medium">Job Title</p>
+            <p className="text-gray-600">
+              {contract ? contract.job_title : ""}
+            </p>
+          </div>
+          <div className="border-b py-4">
+            <p className="font-medium">Job Description</p>
+            <p className="text-gray-600">
+              {contract ? contract.job_description : ""}
+            </p>
+          </div>
+          <div className="border-b py-4">
+            <p className="font-medium">Rate ($)</p>
+            <p className="text-gray-600">{contract ? contract.payment : ""}</p>
+          </div>
+          <div className="border-b py-4">
+            <p className="font-medium">Employee Wallet Address</p>
+            <p className="text-gray-600">
+              {contract ? contract.payment_address : ""}
+            </p>
+          </div>
+          <div className="border-b py-4">
+            <p className="font-medium">Contract Status</p>
+            <p className="text-gray-600">
+              {contract ? capitalizeFirst(contract.status) : ""}
+            </p>
+          </div>
+          {isEmployer &&
+            contract &&
+            contract.status === "active" &&
+            contract.payment_status !== "pending" &&
+            contract.payment_status !== "active" && (
+              <div className="my-2">
+                <p className="font-medium">Payment Hash</p>
+                <input
+                  type="text"
+                  placeholder="Submit Blockchain payment Hash"
+                  className="w-full border rounded p-2"
+                />
+                <Button
+                  label="Submit Payment Hash"
+                  onClick={() => handleSendPayment("send")}
+                  style="mt-2"
+                />
               </div>
             )}
-          </div>
-        )}
-        {!isEmployer &&
-          contract &&
-          contract.status === "active" &&
-          contract.payment_status === "pending" && (
-            <Button
-              label="Confirm Payment"
-              onClick={() => handleSendPayment("confirm")}
-            />
+          {!isEmployer && (
+            <div>
+              {acceptingPaymentClicked ? (
+                <p>Processing acceptance...</p>
+              ) : (
+                <div>
+                  {contract && contract.status === "pending" && (
+                    <Button
+                      label="Accept Contract"
+                      onClick={handleEnterAgreement}
+                    />
+                  )}
+                </div>
+              )}
+            </div>
           )}
-      </div>
-    </Wrapper>
+          {!isEmployer &&
+            contract &&
+            contract.status === "active" &&
+            contract.payment_status === "pending" && (
+              <Button
+                label="Confirm Payment"
+                onClick={() => handleSendPayment("confirm")}
+              />
+            )}
+        </div>
+      </Wrapper>
+    </>
   );
 };
 
