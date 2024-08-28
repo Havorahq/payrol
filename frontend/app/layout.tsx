362f70024c94bf8feb1a1213967d67e21647d503
Ololade Otemade <adeniyiotemade@gmail.com>
2024-08-28 22:58:56 +0100

    New datepicker

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -2,7 +2,9 @@
 import React from "react";
 import { Providers } from "./providers";
 import "./globals.css";
-import "rsuite/DatePicker/styles/index.css";
+// import "rsuite/DatePicker/styles/index.css";
+import "rsuite/dist/rsuite.min.css";
+import "react-datepicker/dist/react-datepicker.css";
 
 export default function RootLayout({
   children,
